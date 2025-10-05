93ca488d9916ab8aafe0af7734947ac0b32e0d36
Allen C Kempe <allenck@windstream.net>
2025-10-04 22:57:26 -0400

    adjust stretch factors for qurrydialog window

--- Resources/sql/Sqlite3/sqlite3_create_tables.sql
+++ Resources/sql/Sqlite3/sqlite3_create_tables.sql
@@ -165,5 +165,22 @@ CREATE TABLE if not exists `Terminals` (
   constraint pk PRIMARY KEY (`Route`,`Name`,`StartDate`,`EndDate`)
 );
 
+CREATE TABLE IF NOT EXISTS `StreetDef` (
+  `StreetId` INTEGER   AUTO_INCREMENT ,
+  `Street` varchar(60) NOT NULL DEFAULT '' ,
+  `Location` varchar(30),
+  `StartDate` date NOT NULL DEFAULT '2050-01-01',
+  `EndDate` date,
+  `StartLatLng` `text`,
+  `EndLatLng` `text`,
+  `Length` decimal(15,5) NOT NULL DEFAULT 0,
+  `Bounds` `text`,
+  `Segments` text,
+  `Comment` `text`,
+  `Seq` INT NOT NULL DEFAULT 1,
+  `lastUpdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY(`StreetId`, `Seq`),
+  UNIQUE (`Street`, `StartDate`)
+);
 
 analyze;
