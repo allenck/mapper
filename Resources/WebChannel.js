1f7f2561badea0e2c47c8c336ef65c123f454e03
Allen C Kempe <allenck@windstream.net>
2026-09-07 20:37:30 -0400

    remove inoperative closeWindow called on close

--- Resources/WebChannel.js
+++ Resources/WebChannel.js
@@ -17,8 +17,8 @@ var socket = new WebSocket(baseUrl);
 socket.onclose = function()
 {
  console.error("web channel closed");
-    //alert("connection to application has been closed. ");
-    closeWindow();
+    alert("connection to application has been closed. ");
+    //closeWindow();
 };
 socket.onerror = function(error)
 {
